f6cd4eb7857626446d96c7943d4e98c84cb71673
Asakura Mizu <asakuramizu111@gmail.com>
2025-06-27 01:58:23 +0800

    fix: send SIGSEGV on page fault

--- src/mm.rs
+++ src/mm.rs
@@ -5,9 +5,31 @@ use axhal::{
 };
 use axsignal::{SignalInfo, Signo};
 use axtask::current;
-use linux_raw_sys::general::{RLIMIT_STACK, SI_KERNEL, SIGSEGV};
-use starry_api::{do_exit, signal::send_signal_process};
+use linux_raw_sys::general::{RLIMIT_STACK, SI_KERNEL};
+use starry_api::signal::send_signal_process;
 use starry_core::{mm::is_accessing_user_memory, task::StarryTaskExt};
+
+fn handle_user_page_fault(
+    ext: &StarryTaskExt,
+    vaddr: VirtAddr,
+    access_flags: MappingFlags,
+) -> bool {
+    if (starry_config::USER_STACK_TOP - starry_config::USER_STACK_SIZE
+        ..starry_config::USER_STACK_TOP)
+        .contains(&vaddr.as_usize())
+    {
+        // Stack extension, check rlimit
+        let rlim = &ext.process_data().rlim.read()[RLIMIT_STACK];
+        let size = starry_config::USER_STACK_TOP - vaddr.as_usize();
+        if size as u64 > rlim.current {
+            return false;
+        }
+    }
+    ext.process_data()
+        .aspace
+        .lock()
+        .handle_page_fault(vaddr, access_flags)
+}
 
 #[register_trap_handler(PAGE_FAULT)]
 fn handle_page_fault(vaddr: VirtAddr, access_flags: MappingFlags, is_user: bool) -> bool {
@@ -21,34 +43,20 @@ fn handle_page_fault(vaddr: VirtAddr, access_flags: MappingFlags, is_user: bool)
 
     let curr = current();
     let ext = StarryTaskExt::of(&curr);
-    if (starry_config::USER_STACK_TOP - starry_config::USER_STACK_SIZE
-        ..starry_config::USER_STACK_TOP)
-        .contains(&vaddr.as_usize())
-    {
-        // Stack extension, check rlimit
-        let rlim = &ext.process_data().rlim.read()[RLIMIT_STACK];
-        let size = starry_config::USER_STACK_TOP - vaddr.as_usize();
-        if size as u64 > rlim.current {
-            send_signal_process(
-                ext.thread.process(),
-                SignalInfo::new(Signo::SIGSEGV, SI_KERNEL as _),
-            )
-            .expect("Failed to send SIGSEGV");
-        }
-    }
-    if !ext
-        .process_data()
-        .aspace
-        .lock()
-        .handle_page_fault(vaddr, access_flags)
-    {
+
+    if !handle_user_page_fault(ext, vaddr, access_flags) {
         warn!(
-            "{} ({:?}): segmentation fault at {:#x}, exit!",
+            "{} ({:?}): segmentation fault at {:#x}",
             curr.id_name(),
             ext.thread,
             vaddr
         );
-        do_exit(SIGSEGV as _, true);
+        send_signal_process(
+            ext.thread.process(),
+            SignalInfo::new(Signo::SIGSEGV, SI_KERNEL as _),
+        )
+        .expect("Failed to send SIGSEGV");
     }
+
     true
 }
