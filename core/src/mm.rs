da6fbf63cdd2855f725d645d089200c1434571ed
Asakura Mizu <asakuramizu111@gmail.com>
2025-06-29 01:00:02 +0800

    chore: reduce warning on la

--- core/src/mm.rs
+++ core/src/mm.rs
@@ -9,7 +9,7 @@ use axhal::{
     mem::virt_to_phys,
     paging::{MappingFlags, PageSize},
 };
-use axmm::{AddrSpace, kernel_aspace};
+use axmm::AddrSpace;
 use kernel_elf_parser::{ELFParser, app_stack_region};
 use memory_addr::{MemoryAddr, PAGE_SIZE_4K, VirtAddr};
 use xmas_elf::{ElfFile, program::SegmentData};
@@ -24,13 +24,13 @@ pub fn new_user_aspace_empty() -> AxResult<AddrSpace> {
 
 /// If the target architecture requires it, the kernel portion of the address
 /// space will be copied to the user address space.
-pub fn copy_from_kernel(aspace: &mut AddrSpace) -> AxResult {
+pub fn copy_from_kernel(_aspace: &mut AddrSpace) -> AxResult {
     #[cfg(not(any(target_arch = "aarch64", target_arch = "loongarch64")))]
     {
         // ARMv8 (aarch64) and LoongArch64 use separate page tables for user space
         // (aarch64: TTBR0_EL1, LoongArch64: PGDL), so there is no need to copy the
         // kernel portion to the user page table.
-        aspace.copy_mappings_from(&kernel_aspace().lock())?;
+        _aspace.copy_mappings_from(&axmm::kernel_aspace().lock())?;
     }
     Ok(())
 }
