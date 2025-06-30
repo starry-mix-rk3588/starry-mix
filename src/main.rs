00dde87651a2aa8655507b4c4c8572d51c50b111
mivik <mivikq@gmail.com>
2025-06-30 16:08:47 +0800

    fix: loongarch elf cache

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,20 @@ mod entry;
 mod mm;
 mod syscall;
 
+#[cfg(target_arch = "riscv64")]
 const CACHED_ELFS: &[&str] = &[
     "/musl/busybox",
     "/glibc/busybox",
     "/musl/lib/libc.so",
     "/glibc/lib/ld-linux-riscv64-lp64d.so.1",
+];
+
+#[cfg(target_arch = "loongarch64")]
+const CACHED_ELFS: &[&str] = &[
+    "/musl/busybox",
+    "/glibc/busybox",
+    "/musl/lib/libc.so",
+    "/glibc/lib/ld-linux-loongarch-lp64d.so.1",
 ];
 
 #[unsafe(no_mangle)]
